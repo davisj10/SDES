1bd6d82f6faee5f76b7e2bb8f638dd922af3e7cb
tcrobin18 <32551559+tcrobin18@users.noreply.github.com>
2018-11-10 22:25:07 -0500

    Fully functioning

--- src/sdes/SDES.java
+++ src/sdes/SDES.java
@@ -1,7 +1,6 @@
 package sdes;
 
-import org.omg.PortableInterceptor.INACTIVE;
-
+import java.nio.charset.Charset;
 import java.util.Scanner;
 
 /**
@@ -14,6 +13,7 @@ import java.util.Scanner;
  */
 public class SDES {
 
+    //Various permutations used by expPerm
 	private static int[] IP = {1, 5 , 2, 0, 3, 7, 4, 6};
 	private static int[] IPINV = {3, 0, 2, 4, 6, 1, 7, 5};
 	private static int[] K1 = {0, 6, 8, 3, 7, 2, 9, 5};
@@ -21,8 +21,27 @@ public class SDES {
 	private static int[] P4 = {1, 3, 2, 0};
 	private static int[] EP = {3, 0, 1, 2, 1, 2, 3, 0};
 
+	//The 2 permuted 8-bit keys from the entered 10 bit key
 	private boolean[] k1;
 	private boolean[] k2;
+
+	//S0 block
+	private static final boolean[][][] s0 = {
+			{{false, true},{false, false},{true, true},{true, false}},
+			{{true, true},{true,false},{false,true},{false,false}},
+			{{false,false},{true,false},{false,true},{true,true}},
+			{{true,true},{false,true},{true,true},{true,false}}
+	};
+
+	//S1 block
+	private static final boolean[][][] s1 = {
+			{{false,false,},{false,true},{true,false},{true,true}},
+			{{true,false},{false,false},{false,true},{true,true}},
+			{{true,true},{false,false},{false,true},{false,false}},
+			{{true,false},{false,true},{false,false},{true,true}}
+	};
+
+
 
 	public SDES() {} //end default constructor
 
@@ -32,16 +51,20 @@ public class SDES {
 	 * @param inp A bit array, max length is 8 bits
 	 * @return Byte represented my the given bit array
 	 */
-	public int bitArrayToByte(boolean[] inp) {
-		byte result;
+	public byte bitArrayToByte(boolean[] inp) {
+		 byte result;
 		 StringBuilder sr = new StringBuilder();
+		 //Convert boolean values of inp to 1's and zero's
 		 for (boolean b : inp){
-		 	if (b == true){
+		 	if (b){
 		 		sr.append(1);
 			}
 			else sr.append(0);
 		 }
 
+		 //Negative number conversion to byte was not working, so convert
+        //negative numbers into their 2's complement positive counterpart
+        //Prepend a '-' for the byte conversion
 		 if (sr.charAt(0) == '1'){
 		 	int i = sr.length() - 1;
 		 	while (sr.charAt(i) == '0'){
@@ -60,6 +83,7 @@ public class SDES {
 
 		 String byteString = sr.toString();
 
+		 //Parse the string of 1's and 0's to a byte
 		 result = Byte.parseByte(byteString, 2);
 
 
@@ -73,8 +97,7 @@ public class SDES {
 	 * @return The characters as a string
 	 */
 	public String byteArrayToString(byte[] inp) {
-		String byteChars = new String(inp);
-		return byteChars;
+		return new String(inp);
 	}
 
 	/**
@@ -88,16 +111,19 @@ public class SDES {
 		boolean[] result = new boolean[size];
 		StringBuilder sr = new StringBuilder();
 		String test = Integer.toBinaryString(b);
+		//Trim negative bytes down from 32 bits to the appropriate size
 		if (b < 0){
 			test = test.substring(test.length() - size);
 		}
 		sr.append(test);
 
+		//Extend the bit string to the appropriate length
 		while (sr.length() < size){
 			sr.insert(0, 0);
 		}
 		String bitString = sr.toString();
 
+		//Convert the 1's and 0's of the bit strings to boolean values
 		for (int i = 0; i < size; i++){
 			result[i] = bitString.charAt(i) == '1';
 		}
@@ -116,31 +142,73 @@ public class SDES {
 		int size = x.length + y.length;
 		boolean[] result = new boolean[size];
 
+		//Copy the x array to the result array, starting at 0
 		System.arraycopy(x, 0 ,result, 0, x.length);
 
+		//Copy the y array to the resutl array, starting at the end of the x array
 		System.arraycopy(y, 0, result, x.length, y.length);
 
 		return result;
 	}
-	
-	public byte[] decrypt(byte[] cipher) {
 
+	/**
+	 *
+	 * @author Justin Davis
+	 * @param cipher Array of bytes to be Decrypted
+	 * @return Decrypted array of bytes
+	 */
+	public byte[] decrypt(byte[] cipher) {
+		int size = cipher.length;
+		for(int i = 0; i < size; i++) {
+			cipher[i] = decryptByte(cipher[i]);
+		}
 		return cipher;
 	}
-	
+
+	/**
+	 *
+	 * @author Justin Davis
+	 * @param b Byte to be decrypted
+	 * @return Decrypted byte
+	 */
 	public byte	decryptByte(byte b) {
-
-		return b;
+		boolean[] temp = byteToBitArray(b, 8);
+		temp = expPerm(temp, IP);
+		temp = f(temp, k2);
+		temp = concat(rh(temp), lh(temp)); //swap left and right halves
+		temp = f(temp, k1);
+		temp = expPerm(temp, IPINV);
+		return bitArrayToByte(temp);
 	}
-	
+
+	/**
+	 *
+	 * @author Justin Davis
+	 * @param msg Byte array to be encrypted
+	 * @return Encrypted byte array
+	 */
 	public byte[] encrypt(String msg) {
-
-		return null;
+		byte[] cipher = msg.getBytes();
+		int size = cipher.length;
+		for(int i = 0; i < size; i++) {
+			cipher[i] = encryptByte(cipher[i]);
+		}
+		return cipher;
 	}
-	
-	public byte	encryptByte(byte b) {
 
-		return b;
+	/**
+	 * @author Justin Davis
+	 * @param b Byte to be encrypted
+	 * @return Encrypted byte
+	 */
+	public byte	encryptByte(byte b) {
+		boolean[] temp = byteToBitArray(b, 8);
+		temp = expPerm(temp, IP);
+		temp = f(temp, k1);
+		temp = concat(rh(temp), lh(temp)); //swap left and right halves
+		temp = f(temp, k2);
+		temp = expPerm(temp, IPINV);
+		return bitArrayToByte(temp);
 	}
 
 	/**
@@ -156,20 +224,48 @@ public class SDES {
 	public boolean[] expPerm(boolean[] inp, int[] epv) throws IndexOutOfBoundsException {
 		boolean[] result = new boolean[epv.length];
 		for (int i = 0; i < epv.length; i++){
+		    //epv[] holds array index values that are used to pull indexes from inp into result
 			result[i] = inp[epv[i]];
 		}
 
 		return result;
 	}
-	
+
+    /**
+     * This is the 'round' function
+     * It is its own inverse
+     * f(x,k) = L(x) xor F(R(x),k)) || R(x)
+     * @param x Byte to be decrypted
+     * @param k Key for the current round
+     * @return Result of the round' function
+     */
 	public boolean[] f(boolean[] x, boolean[] k) {
-
-		return k;
+		boolean[] left = lh(x);
+		boolean[] right = rh(x);
+		//Concatenate the left half of x XOR'd with the feistel network result of the right half of x with the current key
+        //with the right half of x
+		return concat((xor(left, feistel(k, right))), right);
 	}
-	
-	public boolean[] feistel(boolean[] k, boolean[] x) {
 
-		return x;
+    /**
+     * F(k,x) is a Feistel function F(k,x) = P4(s0(L(k xor EP(x))) || s1 (R (k xor EP(x)))
+     * @param k Key for the current round
+     * @param x Left half of the byte being decrypted
+     * @return The result of the Feistel function
+     */
+	public boolean[] feistel(boolean[] k, boolean[] x) {
+	    //Get the left and right halves of k xor EP(x)
+		boolean[] lKXorEp = lh(xor(k, expPerm(x, EP)));
+		boolean[] rKXorEp = rh(xor(k, expPerm(x, EP)));
+
+		//Use the boolean values calculated for KxorX to determine the index values for the S blocks
+		int i1 = Integer.parseInt(((lKXorEp[0]) ? "1" : "0") + ((lKXorEp[3] ? "1" : "0")), 2);
+		int i2 = Integer.parseInt(((lKXorEp[1]) ? "1" : "0") + ((lKXorEp[2] ? "1" : "0")), 2);
+		int i3 = Integer.parseInt(((rKXorEp[0]) ? "1" : "0") + ((rKXorEp[3] ? "1" : "0")), 2);
+		int i4 = Integer.parseInt(((rKXorEp[1]) ? "1" : "0") + ((rKXorEp[2] ? "1" : "0")), 2);
+
+		//Calculate the P4 permutation of the concatenated S block values
+		return expPerm(concat(s0[i1][i2], s1[i3][i4]), P4);
 	}
 
 	/**
@@ -179,13 +275,16 @@ public class SDES {
 	 * @param scanner Scanner used to receive key from user
 	 */
 	public void	getKey10(java.util.Scanner scanner) {
+	    //Get the key from the user
 		String keyString = scanner.nextLine();
-		keyString.replaceAll(" ", "");
-		keyString.split("");
+		//remove any spaces entered by the user
+		keyString = keyString.replaceAll(" ", "");
 		boolean[] key = new boolean[keyString.length()];
+		//Convert 1's and 0's entered by user into boolean values
 		for (int i = 0; i < keyString.length(); i++){
 			key[i] = keyString.charAt(i) == '1';
 		}
+		//Set the permuted keys for this encryption/decryption session
 		this.k1 = expPerm(key, K1);
 		this.k2 = expPerm(key, K2);
 	}
@@ -199,6 +298,7 @@ public class SDES {
 	public boolean[] lh(boolean[] inp) {
 		boolean[] left = new boolean[4];
 
+		//Copy the left half of inp to left
 		System.arraycopy(inp, 0, left, 0, 4);
 
 		return left;
@@ -213,17 +313,40 @@ public class SDES {
 	public boolean[] rh(boolean[] inp) {
 		boolean[] right = new boolean[4];
 
+		//Copy right half of inp to right
 		System.arraycopy(inp, 4, right, 0, 4);
 
 		return right;
 	}
-	
+
+	/**
+	 * May need to use stdout!!!
+	 *
+	 * @author Justin Davis
+	 * @param inp Boolean array to print to screen
+	 */
 	public void	show(boolean[] inp) {
-
+		StringBuilder output = new StringBuilder();
+		int size = inp.length;
+		for (int i = 0; i < size; i++) {
+			output.append(inp[i] ? 1 : 0);
+		}
+		System.out.print(output.toString());
 	}
-	
-	public void	show(byte[] byteArray) {
 
+	/**
+	 * May need to use stdout!!!
+	 *
+	 * @author Justin Davis
+	 * @param byteArray Byte array to print to the screen
+	 */
+	public void	show(byte[] byteArray) {
+		StringBuilder sb = new StringBuilder();
+		for (byte b : byteArray){
+			sb.append(b);
+			sb.append(" ");
+		}
+		System.out.println(sb.toString());
 	}
 
 	/**
@@ -236,6 +359,7 @@ public class SDES {
 	public boolean[] xor(boolean[] x, boolean[] y) {
 		boolean[] result = new boolean[x.length];
 
+		//XOR the corresponding values of x and y
 		for (int i = 0; i < x.length; i++){
 			result[i] = x[i] ^ y[i];
 		}
